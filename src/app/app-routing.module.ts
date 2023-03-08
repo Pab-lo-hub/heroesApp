97895d206c99d9c52eb4da699c1fbb2aad63283a
Pab-lo-hub <salaberry.pablo@gmail.com>
2023-03-08 13:08:14 -0300

    Routing agregados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorpageComponent } from './shared/errorpage/errorpage.component';
+import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then ( m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then ( m => m.HeroesModule)
+  },
   {
     path: '404',
     component: ErrorpageComponent
